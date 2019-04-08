d9a6d169d3b3efc75b88dcaa29269a8406fd53e7
mshan10 <mshan@nd.edu>
2019-04-08 16:53:44 -0400

    finished logic

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,14 +1,27 @@
 <template>
   <div class="hello">
     <h1>{{ total_enrolled | currency }}</h1>
+    <input v-model="total_enrolled" v-on:input="calculateBA">
+    <input v-model="average_cost" v-on:input="calculateBA">
+    <!-- <ul>
+      <li>{{ total_enrolled }}</li>
+      <li>{{ average_cost }}</li>
+      <li>{{ mode }}</li>
+    </ul> -->
     <ul>
-      <button v-on:click="addNumbers">Add</button>
+      <li>{{ removed1 }} <span v-if="mode"> - {{ removed2 }}</span></li>
+      <li>{{ savings1 | currency }} <span v-if="mode"> - {{ savings2 | currency }}</span></li>
+    </ul>
+    <ul>
+      <mdc-button raised v-on:click="changeMode(0)">Below Average</mdc-button>
+      <mdc-button raised v-on:click="changeMode(1)">Average</mdc-button>
+      <mdc-button raised v-on:click="changeMode(2)">Above Average</mdc-button>
     </ul>
   </div>
 </template>
 
 <script>
-import Vue2Filters from 'vue2-filters'
+// import VueMDCButton from 'vue-mdc-adapter/button'
 export default {
   name: 'HelloWorld',
   props: {
@@ -18,16 +31,24 @@ export default {
     return {
       total_enrolled: 0,
       average_cost: 0,
-      removed: 0,
-      savings: 0
+      removed1: 0,
+      removed2: 0,
+      savings1: 0,
+      savings2: 0,
+      mode: 0
     }
   },
   methods: {
-    calculate: function() {
-      removed = total_enrolled * .02
-      savings = removed * average_cost
-      // this.total_enrolled = 54321
+    calculateBA: function() {
+      this.removed1 = (!this.mode) ? (this.total_enrolled * .02) : (this.total_enrolled * .03 * this.mode)
+      this.removed2 = this.total_enrolled * .05 * this.mode
+      this.savings1 = this.removed1 * this.average_cost
+      this.savings2 = (!this.mode) ? 0 : (this.removed2 * this.average_cost)
       console.log("add numbers")
+    },
+    changeMode: function(num) {
+      this.mode = num
+      this.calculateBA()
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import Vue2Filters from 'vue2-filters'
 import VueMDCAdapter from 'vue-mdc-adapter'
+import "vue-mdc-adapter/dist/vue-mdc-adapter.css";
 
 Vue.config.productionTip = false
 Vue.use(Vue2Filters)
